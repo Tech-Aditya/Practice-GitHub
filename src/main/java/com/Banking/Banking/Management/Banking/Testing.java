784cb25847632627eebbb24f6790354495fdfb62
aditya.s <srivastavaditya09@gmail.com>
2024-02-21 16:52:48 +0530

    Initial Commit

---
+++ src/main/java/com/Banking/Banking/Management/Banking/Testing.java
@@ -0,0 +1,9 @@
+package com.Banking.Banking.Management.Banking;
+
+public class Testing {
+    public static void main(String[] args) {
+        
+    System.out.println("This is Just For Testing Purpose");
+    
+}
+}
